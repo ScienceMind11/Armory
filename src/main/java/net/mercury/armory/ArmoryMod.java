deb0474643fdbcccca7d58f9784b0ca4fde1d9be
ScienceMind11 <sciencemind@icloud.com>
2024-02-01 18:45:25 -0500

    Boilerplate to get things working :)

---
+++ src/main/java/net/mercury/armory/ArmoryMod.java
@@ -0,0 +1,20 @@
+package net.mercury.armory;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArmoryMod implements ModInitializer {
+
+	public static final String NAME = "Armory";
+	public static final String ID = "armory";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+
+}
